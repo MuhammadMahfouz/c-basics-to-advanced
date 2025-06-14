08693f1184e28abe5e31ccc8b3453b73376accdb
MuhammadMahfouz <muhammadmahfouzofficial@gmail.com>
2025-06-14 21:09:34 +0300

    Add C Program to convert from Celsius to Fahrenheit

---
+++ basic-c-programs/convert_celsius_to_fahrenheit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// This program converts Celsius to Fahrenheit
+int main() 
+{
+    int cel;
+    for (cel = 0; cel <= 100; cel = cel + 20)
+        printf("%4d %6.1f\n", cel, ((cel * (9.0/5.0)) + 32.0));
+}
